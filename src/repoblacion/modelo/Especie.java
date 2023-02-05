2e4084a9868b64b84bd316383b33fc153646b18d
Konstantin <constantinopladwar@gmail.com>
2023-02-05 21:43:38 +0100

    2.Cree enumerado Especie

---
+++ src/repoblacion/modelo/Especie.java
@@ -0,0 +1,17 @@
+package repoblacion.modelo;
+
+public enum Especie {
+
+ALAMO("Alamo"), ENCINA("Encina"), CASTAÑO("Castaño"), CIPRES("Ciprés"), PINO("Pino piñonero"),ROBLE("Roble"),OLIVO("Olivo");
+	
+	private String cadenaAMostrar;
+
+	private Especie(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
+	public String toString() {
+		return cadenaAMostrar;
+	}
+	
+}
